3ed957332fee727331363fdd595be62b6c2de871
Flávia Correia <fllaviacorreia@outlook.com>
2022-12-17 17:57:05 -0300

    include jpa and equals + hash code

--- src/main/java/com/projects/library/domain/model/PublishingCompanyModel.java
+++ src/main/java/com/projects/library/domain/model/PublishingCompanyModel.java
@@ -1,15 +1,30 @@
 package com.projects.library.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity(name = "publishing_companies")
 public class PublishingCompanyModel {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
+	
 	private String city;
+
 	private String state;
+
 	private String country;
 }
