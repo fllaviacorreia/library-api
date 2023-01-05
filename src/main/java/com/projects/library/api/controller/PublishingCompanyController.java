4e5ca74e7a5764f3889226a43c7832e012b26491
Flávia Correia <fllaviacorreia@outlook.com>
2023-01-05 15:51:50 -0300

    create crud

--- src/main/java/com/projects/library/api/controller/PublishingCompanyController.java
+++ src/main/java/com/projects/library/api/controller/PublishingCompanyController.java
@@ -2,24 +2,73 @@ package com.projects.library.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projects.library.domain.model.PublishingCompanyModel;
+import com.projects.library.domain.repository.PublishingCompanyRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/publishing-companies")
 public class PublishingCompanyController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	@GetMapping("/publishing-companies")
+	private PublishingCompanyRepository pCompanyRepository;
+	
 	public List<PublishingCompanyModel> getPublishingCompanies(){
-		return entityManager
-				.createQuery("from publishing_companies", PublishingCompanyModel.class)
-				.getResultList();
+		return pCompanyRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PublishingCompanyModel> getPublishingCompany(@PathVariable Long id){
+		return pCompanyRepository.findById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PublishingCompanyModel createPublishingCompany(@RequestBody PublishingCompanyModel pCompany) {
+		return pCompanyRepository.save(pCompany);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<PublishingCompanyModel> editPublishingCompany(
+			@PathVariable Long id, 
+			@RequestBody PublishingCompanyModel pCompany){
+		
+		if(!pCompanyRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		pCompany.setId(id);
+		pCompany = pCompanyRepository.save(pCompany);
+		
+		return ResponseEntity.ok(pCompany);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<PublishingCompanyModel> deletePublishingCompany(@PathVariable Long id){
+		
+		if(!pCompanyRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		pCompanyRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
+		
 	}
 }
+
+
