681d07b0922152057e07ec21175ff72ce9760cee
Flávia Correia <fllaviacorreia@outlook.com>
2023-01-12 11:31:14 -0300

    include validation in model of pub. company

--- src/main/java/com/projects/library/domain/model/PublishingCompanyModel.java
+++ src/main/java/com/projects/library/domain/model/PublishingCompanyModel.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,11 +24,20 @@ public class PublishingCompanyModel {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 200)
 	private String name;
-	
+
+	@NotBlank
+	@Size(max = 50)
 	private String city;
 
+	@NotBlank
+	@Size(max = 5)
 	private String state;
 
+
+	@NotBlank
+	@Size(max = 100)
 	private String country;
 }
