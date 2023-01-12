78b1b40a76756823e3ea55ab6a7850f9eba36a1e
Flávia Correia <fllaviacorreia@outlook.com>
2023-01-12 17:32:33 -0300

    include class field

---
+++ src/main/java/com/projects/library/api/exception/Field.java
@@ -0,0 +1,12 @@
+package com.projects.library.api.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Field {
+
+	private String field;
+	private String message;
+}
