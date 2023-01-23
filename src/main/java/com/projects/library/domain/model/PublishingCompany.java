bccbe5adfe643d62db6b0d2247a84ad35607566c
Flávia Correia <fllaviacorreia@outlook.com>
2023-01-23 11:28:54 -0300

    remove Model from PCompany

---
+++ src/main/java/com/projects/library/domain/model/PublishingCompany.java
@@ -0,0 +1,43 @@
+package com.projects.library.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+@Table(name = "publishing_companies")
+public class PublishingCompany {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max = 200)
+	private String name;
+
+	@NotBlank
+	@Size(max = 50)
+	private String city;
+
+	@NotBlank
+	@Size(max = 5)
+	private String state;
+
+
+	@NotBlank
+	@Size(max = 100)
+	private String country;
+}
