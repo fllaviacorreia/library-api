9c30af038a327bdff07ea4466c35a0dbbb8025fd
Flávia Correia <fllaviacorreia@outlook.com>
2023-01-23 11:28:40 -0300

    remove Model from PCompany

--- src/main/java/com/projects/library/domain/repository/PublishingCompanyRepository.java
+++ src/main/java/com/projects/library/domain/repository/PublishingCompanyRepository.java
@@ -3,10 +3,10 @@ package com.projects.library.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.projects.library.domain.model.PublishingCompanyModel;
+import com.projects.library.domain.model.PublishingCompany;
 
 @Repository
-public interface PublishingCompanyRepository extends JpaRepository<PublishingCompanyModel, Long>{
+public interface PublishingCompanyRepository extends JpaRepository<PublishingCompany, Long>{
 
 }
 
